d1ec315d080089ea3086224c316d3df80e24c966
Colin Shea <colin@evaryont.me>
2009-12-19 23:16:02 -0500

    More tests! Check .size and [] works.

--- test/test.rb
+++ test/test.rb
@@ -4,3 +4,5 @@ assert { @mast.projects == ["coding", "testing", "documenting"] }
 assert { @mast.contexts == ["work", "phone", "home"] }
 assert { @mast.find(:context => "work") == ["work"] }
 assert { @mast.find(:project => "coding") == ["work"] }
+assert { @mast.size == 3 }
+assert { @mast[0] == "blah" }
