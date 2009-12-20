bfd116ae947081f545001d04600ae3b8e6d37640
Colin Shea <colin@evaryont.me>
2009-12-19 23:27:56 -0500

    Group and organize (via comments) the tests.
    Also, test for non-existing contexts and projects as well.

--- test/test.rb
+++ test/test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
+# Test searching and caching.
 assert { @mast.projects == ["coding", "testing", "documenting"] }
 assert { @mast.contexts == ["work", "phone", "home"] }
-assert { @mast.find(:context => "work") == ["work"] }
-assert { @mast.find(:project => "coding") == ["work"] }
+
+# Finding existing items
+assert { @mast.find_context("work") == ["work"] }
+assert { @mast.find_project("coding") == ["work"] }
+
+# Finding non-existing items
+assert { @mast.find_context("nosuchthing") == [] }
+assert { @mast.find_project("thisprojectdoesntexist") == [] }
+
+# General utility methods.
 assert { @mast.size == 3 }
-assert { @mast[0] == "blah" }
+assert { @mast[0] == "Finish mastadon +coding" }
