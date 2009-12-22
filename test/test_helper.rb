a1b20edfcab39a6695ff5bfb0d6d829cf5758101
Colin Shea <colin@evaryont.me>
2009-12-22 13:59:07 -0500

    Expand the sample todo coverage.
    Projects and contexts are valid for all characters except spaces. Fun.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,9 +18,20 @@ end
 require File.dirname(__FILE__) + "/../lib/mastodon.rb"
 
 SAMPLE_TODOS = [
-    "Finish mastadon +coding @work",
-    "Spec 100% +testing @people",
-    "Document 100% +documenting"
+    "Todo @con +proj",
+    "left @ right @con",
+    "user@server email",
+    "Multiple projects +proj +proj2",
+    "Multiple contexts @con @con2",
+    "(A) Priority",
+    "(Z) All the way to Z",
+    "Not a priority: (A)",
+    "(Q) All mixed together @con +proj",
+    "Advanced contexts @con@phone @con/foo @c_o_n @c-o-n @@con",
+    "Advanced projects +con+phone +con/foo +c_o_n +c-o-n ++proj",
+    "Mixed symbols: +@proj @+con",
+    "@con Leading with a context",
+    "+proj Leading with a project"
 ] unless Kernel.const_defined? :SAMPLE_TODOS
 @mast = Mastodon.new(SAMPLE_TODOS)
 
