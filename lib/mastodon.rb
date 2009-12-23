b2d84e874219937d0fe3a24dd696af43fd89a01d
Colin Shea <colin@evaryont.me>
2009-12-22 21:13:42 -0500

    A rather massive restructing of Mastodon, now it uses classes!

--- lib/mastodon.rb
+++ lib/mastodon.rb
@@ -2,47 +2,57 @@ require 'mastodon/todo'
 require 'mastodon/version'
 
 class Mastodon
+    # A context is: An at-sign (@) followed by one or more non-whitespace characters.
+    Context_Regex  = /@([^\s]+)/
+    # A project is: An plus sign (+) followed by one or more non-whitespace characters.
+    Project_Regex  = /\+([^\s]+)/
+    # A priority is: A capital letter (A-Z) surrounded by parentheses, at the beginning of the line.
+    Priority_Regex = /^\(([A-Z])\)\s/
+
+    attr_reader :contexts, :projects, :todos
 
     # +todo+: An array of strings (each being an individual todo item)
-    def initialize(todo)
-        @lines = todo.map{|line| line.strip}
+    def initialize(todos)
+        parse! todos.map{|line| line.strip}
     end
 
-# contexts {{{
-    Context_Regex = /@([A-Za-z]+)/
-    # The contexts in the todo file
-    def contexts
-        @contexts ||= contexts!
-    end
+    def parse!(todos)
+        @contexts = Set.new
+        @projects = Set.new
+        @todos = []
 
-    # Loop through each line and find the project context with it
-    def contexts!
-        contexts = []
-        @lines.map do |todo|
-            matchdata = todo.match(Context_Regex)
-            contexts << matchdata[1] if matchdata
+        todos.each do |todo|
+            # Looping through the string, find the next context/project. Remove
+            # it from the string. Add it to the correct set. Then clear any
+            # remaining whitespace.
+
+            current_contexts = []
+            current_projects = []
+
+            until ((context = todo[Context_Regex]).nil?)
+                index = todo.index(context)
+                todo[index..(index+context.length)] = ""
+                current_contexts << context.match(Context_Regex)[1]
+                contexts << context.match(Context_Regex)[1]
+            end
+
+            until ((project = todo[Project_Regex]).nil?)
+                index = todo.index(project)
+                todo[index..(index+project.length)] = ""
+                current_projects << project.match(Project_Regex)[1]
+                projects << project.match(Project_Regex)[1]
+            end
+
+            priority = todo[Priority_Regex]
+            unless priority.nil?
+                todo[0..4] = "" # We know 0..4 as the priority begins the line (0) is always 1 character long (1) surrounded by parentheses (+2) and is followed by a space (+1).
+                priority = priority.match(Priority_Regex)[1]
+            end
+
+            todo.strip!
+            @todos << Mastodon::Todo.new(todo, current_contexts, current_projects, priority)
         end
-        contexts
     end
-# }}}
-
-# projects {{{
-    Project_Regex = /\+([A-Za-z]+)/ # /\+(".*?")/
-    # The projects in the todo file
-    def projects
-        @projects ||= projects!
-    end
-
-    # Loop through each line and find the project associated with it
-    def projects!
-        projects = []
-        @lines.map do |todo|
-            matchdata = todo.match(Project_Regex)
-            projects << matchdata[1] if matchdata
-        end
-        projects
-    end
-# }}}
 
     # How many todos there are.
     def size
@@ -51,7 +61,7 @@ class Mastodon
 
     # Get an individual todo. The id and line number are the same.
     def [](id)
-        @lines[id]
+        @todos[id]
     end
 
     # Find all todo's that have the context @+context+
