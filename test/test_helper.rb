3611d120dd608db83ba9f7e118d350608d38e41a
Colin Shea <colin@evaryont.me>
2009-12-22 21:33:50 -0500

    Move the todo list into a separate file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,22 +17,7 @@ end
 # Include the library
 require File.dirname(__FILE__) + "/../lib/mastodon.rb"
 
-SAMPLE_TODOS = [
-    "Todo @con +proj",
-    "left @ right @con",
-    "user@server email",
-    "Multiple projects +proj +proj2",
-    "Multiple contexts @con @con2",
-    "(A) Priority",
-    "(Z) All the way to Z",
-    "Not a priority: (A)",
-    "(Q) All mixed together @con +proj",
-    "Advanced contexts @con@phone @con/foo @c_o_n @c-o-n @@con",
-    "Advanced projects +con+phone +con/foo +c_o_n +c-o-n ++proj",
-    "Mixed symbols: +@proj @+con",
-    "@con Leading with a context",
-    "+proj Leading with a project"
-] unless Kernel.const_defined? :SAMPLE_TODOS
+SAMPLE_TODOS = File.readlines(File.join(File.dirname(__FILE__), "todo.txt")) unless Kernel.const_defined? :SAMPLE_TODOS
 @mast = Mastodon.new(SAMPLE_TODOS)
 
 include Nanotest
