07824da323a70370b4dc7db2d7318455baab8ba2
Colin Shea <colin@evaryont.me>
2009-12-29 15:09:01 -0500

    Get the tests running. But failling.

--- test/mastodon_test.rb
+++ test/mastodon_test.rb
@@ -1,9 +1,8 @@
 require 'teststrap'
 
-@todos = File.readlines(File.join(File.dirname(__FILE__), "todo.txt"))
-
 context "Mastodon" do
     setup do
+        @todos = File.readlines(File.join(File.dirname(__FILE__), "todo.txt"))
         Mastodon.new(@todos)
     end
 
@@ -22,22 +21,56 @@ context "Mastodon" do
         topic.find_project("thisprojectdoesntexist") == []
     end
 
-    asserts { topic.find_context("con2").size  > 0 }
-    asserts { topic.find_context("con2").first.text == "Multiple contexts" }
-    asserts { topic.find_project("proj2").size > 0 }
-    asserts { topic.find_project("proj2").first.text == "Multiple projects" }
+    asserts "Context size should be greater than zero" do
+        topic.find_context("con2").size  > 0
+    end
+    asserts "First todo with the context 'con2' should be 'Multiple contexts'" do
+        topic.find_context("con2").first.text == "Multiple contexts"
+    end
+    asserts "Project size should greater than zero" do
+        topic.find_project("proj2").size > 0
+    end
+    asserts "First todo with the project 'proj2' should be 'Multiple projects'" do
+        topic.find_project("proj2").first.text == "Multiple projects"
+    end
 
-    asserts { (@mast[0].to_s.chars.to_a == SAMPLE_TODOS[0].strip.chars.to_a) and (@mast[0].to_s.length == SAMPLE_TODOS[0].strip.length) }
-    asserts { @mast.size == SAMPLE_TODOS.size }
-    asserts { @mast.projects.is_a? Set }
-    asserts { @mast.contexts.is_a? Set }
-    asserts { @mast[0].is_a? Mastodon::Todo }
-    asserts { @mast[13].contexts = ["con"] }
-    asserts { @mast[14].projects = ["proj"] }
-    asserts { @mast[3].priority.nil?  }
-    asserts { @mast[4].priority.nil?  }
-    asserts { @mast[5].priority = "A" }
-    asserts { @mast[6].priority = "Z" }
-    asserts { @mast[7].priority = "Q" }
-    asserts { @mast[8].priority.nil?  }
+    asserts "same characters and the same length" do
+        (@mast[0].to_s.chars.to_a == SAMPLE_TODOS[0].strip.chars.to_a) and (@mast[0].to_s.length == SAMPLE_TODOS[0].strip.length)
+    end
+    asserts "There should be the same amount of todos" do
+        @mast.size == SAMPLE_TODOS.size
+    end
+    asserts "project should be returned as a Set" do
+        @mast.projects.is_a? Set
+    end
+    asserts "context should be returned as a Set" do
+        @mast.contexts.is_a? Set
+    end
+    asserts "Confirm that the retrieved todos are of type Mastodon::Todo" do
+        @mast[0].is_a? Mastodon::Todo
+    end
+    asserts "13th todo should have the context 'con'" do
+        @mast[13].contexts = ["con"]
+    end
+    asserts "14th todo should have the project 'proj'" do
+        @mast[14].projects = ["proj"]
+    end
+    asserts "Third todo should have no priority" do
+        @mast[3].priority.nil?
+    end
+    asserts "Fourth todo should have no priority" do
+        @mast[4].priority.nil?
+    end
+    asserts "Fifth todo should have priority A" do
+        @mast[5].priority = "A"
+    end
+    asserts "Sixth todo should have priority Z" do
+        @mast[6].priority = "Z"
+    end
+    asserts "Seventh todo should have priority Q" do
+        @mast[7].priority = "Q"
+    end
+    asserts "Eighth todo should have no priority" do
+        @mast[8].priority.nil?
+    end
 end
